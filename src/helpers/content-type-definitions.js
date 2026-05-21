f0f840209d4e6b8561bc0732893a6c56c1f09513
Karol Grochowalski <karol.grochowalski@codewave.pl>
2026-05-21 15:07:57 +0200

    migrate to @flotiq/api

--- src/helpers/content-type-definitions.js
+++ src/helpers/content-type-definitions.js
@@ -1,29 +1,22 @@
 import config from '../configuration/config.js';
 import {getFlotiqApi} from '@flotiq/api';
 import logger from '@flotiq/api/src/logger.js';
-import contentType1 from '../content-type-definitions/contentType1.json' with { type: 'json' };
-import contentType2 from '../content-type-definitions/contentType2.json' with { type: 'json' };
-import contentType3 from '../content-type-definitions/contentType3.json' with { type: 'json' };
-import contentType4 from '../content-type-definitions/contentType4.json' with { type: 'json' };
-import contentType5 from '../content-type-definitions/contentType5.json' with { type: 'json' };
-
+import contentType1 from '../content-type-definitions/contentType1.json' with {type: 'json'};
+import contentType2 from '../content-type-definitions/contentType2.json' with {type: 'json'};
+import contentType3 from '../content-type-definitions/contentType3.json' with {type: 'json'};
+import contentType4 from '../content-type-definitions/contentType4.json' with {type: 'json'};
+import contentType5 from '../content-type-definitions/contentType5.json' with {type: 'json'};
 
 
 export const importer = async (apiKey) => {
     logger.info('# Importing content type definitions to Flotiq');
     const client = getFlotiqApi(config.getApiBaseUrl(), apiKey);
 
-    let contentDefinitions = [
-        contentType1,
-        contentType2,
-        contentType3,
-        contentType4,
-        contentType5,
-    ]
-
-    await Promise.all(contentDefinitions.map(async function (contentDefinition) {
-        await importContentTypedDefinitions(contentDefinition);
-    }));
+    await importContentTypedDefinitions(contentType1);
+    await importContentTypedDefinitions(contentType2);
+    await importContentTypedDefinitions(contentType3);
+    await importContentTypedDefinitions(contentType4);
+    await importContentTypedDefinitions(contentType5);
 
     async function importContentTypedDefinitions(contentDefinition) {
         await client.createOrUpdate(null, structuredClone(contentDefinition));
